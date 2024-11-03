4327da1d4a1206039b96f6fd51e5dc640852ab2d
Caleb Woodbine <calebwoodbine.public@gmail.com>
2024-11-04 11:07:45 +1300

    feat: add LOAD_K8S_DATA env
    configure snoopdb

--- lib/apisnoop.sh
+++ lib/apisnoop.sh
@@ -4,6 +4,11 @@ deploy::apisnoop() {
     echo "deploy::apisnoop"
 
     APISNOOP_IMAGE_TAG="${APISNOOP_VERSION:-v20240626-auditlogger-1.2.12-4-g80e96ac}"
+    LOAD_K8S_DATA="${LOAD_K8S_DATA:-false}"
+
+    if [ "$LOAD_K8S_DATA" = "true" ]; then
+        EXTRA_ARGS="--set extraEnv[0].name=LOAD_K8S_DATA --set-string extraEnv[0].value=true"
+    fi
 
     helm repo add --force-update apisnoop https://kubernetes-sigs.github.io/apisnoop
 
@@ -14,6 +19,7 @@ deploy::apisnoop() {
         --set image.tag="$APISNOOP_IMAGE_TAG" \
         --set service.type="NodePort" \
         --set service.nodePort="30432" \
+        $EXTRA_ARGS \
         snoopdb \
         apisnoop/snoopdb
 
